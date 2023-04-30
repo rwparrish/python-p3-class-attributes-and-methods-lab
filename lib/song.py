0e18054cda5f2686fa009e92200754d0e5ea028a
rwparrish <60946750+rwparrish@users.noreply.github.com>
2023-04-30 11:59:08 -0600

    Sunday mornging coding

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,61 @@
+import ipdb
+
 class Song:
-    pass
+    
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+    
+    def __init__(self, name, artist, genre):
+        self.add_song_to_count()
+        self.add_to_genres(genre)
+        self.add_to_artists(artist)
+        self.add_to_genre_count(genre)
+        self.add_to_artist_count(artist)
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+         
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+    
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+            
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if cls.genre_count.get(genre):
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+            
+            
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if cls.artist_count.get(artist):
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+
+       
+    # populate and dict with keys based list on unqiue genres {item: len(item) for item in my_list}
+    # check the keys in dict for passed in genre
+    # if genre key exists then inciriment value
+    # else add new genre key and value 
+    
+    
+        
+        
+        
+  
+    
